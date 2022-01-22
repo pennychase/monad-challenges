d40ac64c3b1f0cf7a10b4705def74cbf95509cd7
Penny Chase <mpchase@mac.com>
2022-01-22 13:38:37 -0500

    Set 1 problem 3 general pattern

--- monad-challenges-code/src/Set1.hs
+++ monad-challenges-code/src/Set1.hs
@@ -5,25 +5,34 @@ module Set1 where
 
 import MCPrelude
 
+-- Define generalA to capture pattern
+-- Rewrite exercises
+
+type Gen a = Seed -> (a, Seed)
+
+generalA :: (a -> b) -> Gen a -> Gen b
+generalA f g s = 
+    let
+        (x, s') = g s
+    in (f x, s')
+
+genNRand :: Int -> Gen a -> [a]
+genNRand n f = take n $ map fst rands
+    where 
+        rands = f (mkSeed 1) : map (f . snd) rands
 
 -- Random Number Generation
 
 fiveRands :: [Integer]
-fiveRands = nextRand 5 (mkSeed 1) 
-
-nextRand :: Integer -> Seed -> [Integer]
-nextRand cnt seed =
-    if cnt == 0
-        then []
-        else n : nextRand (cnt-1) newSeed
+fiveRands = take 5 $ map fst rands
     where
-        (n, newSeed) = rand seed
+        rands :: [(Integer, Seed)]
+        rands = (0, mkSeed 1) : map (rand . snd) rands
     
 -- Random Character Generation
 
-randLetter :: Seed -> (Char, Seed)
-randLetter seed = (toLetter n, newSeed)
-    where (n, newSeed) = rand seed
+randLetter :: Gen Char 
+randLetter = generalA toLetter rand
 
 randString3 :: String 
 randString3 = nextRandChar 3 (mkSeed 1)
@@ -37,8 +46,6 @@ nextRandChar cnt seed =
         (n, newSeed) = randLetter seed
 
 -- More Generators
-
-type Gen a = Seed -> (a, Seed)
 
 randEven :: Gen Integer 
 randEven seed = (2 * n, newSeed)
@@ -56,3 +63,16 @@ randTen seed = (10 * n, newSeed)
         (n, newSeed) = rand seed
 
 testResult = product $ map (fst . \f -> f (mkSeed 1)) [randEven, randOdd, randTen]
+
+
+
+randEven' :: Gen Integer
+randEven' = generalA (2 *) rand 
+
+randOdd' :: Gen Integer
+randOdd' = generalA (1 +) randEven'
+
+randTen' :: Gen Integer
+randTen' = generalA (10 *) rand
+
+
